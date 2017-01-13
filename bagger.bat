a09e7fdcb3f8de97d4110dc73017ac8d7b8df052
Justin Schell <jmschell@umich.edu>
2017-01-13 15:13:24 -0500

    added bag-it profile

---
+++ bagger.bat
@@ -0,0 +1,61 @@
+{
+  "ordered": [
+    {
+      "UUID": {
+        "fieldRequired": true
+      }
+    },
+    {
+      "Institution facilitating the data capture creation and packaging": {
+        "fieldRequired": true,
+        "defaultValue": "For example: University of Michigan Library"
+      }
+    },
+    {
+      "Date of capture": {
+        "fieldRequired": true
+      }
+    },
+    {
+      "Federal agency data acquired from": {
+        "fieldRequired": true,
+        "defaultValue": "For example: Department of Health and Human Services"
+      }
+    },
+    {
+      "Name of resource": {
+        "fieldRequired": true,
+        "defaultValue": "For example: GOES Evapotranspiration and Drought (GET-D)"
+      }
+    },
+    {
+      "Individual source or seed URL(s)": {
+        "fieldRequired": true
+      }
+    },
+    {
+      "File formats contained in package": {
+        "fieldRequired": true,
+        "defaultValue": "For example: warc.gz, .pdf"
+      }
+    },
+    {
+      "Type(s) of content in package": {
+        "fieldRequired": true,
+        "defaultValue": "For example: datasets, reports, codebook"
+      }
+    },
+    {
+      "Free text description of capture process": {
+        "fieldRequired": true,
+        "defaultValue": "For example: Metadata for reports were captured through web crawl, individual reports were manually downloaded through the search interface."
+      }
+    },
+     {
+        "Name of package creator": {
+          "fieldRequired": false,
+          "defaultValue": "Insert name of person who creaded this package"
+        }
+        }
+  ]
+}
